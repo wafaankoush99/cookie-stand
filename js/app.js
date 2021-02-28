52fa67721594b7fcdc08790e5fe743992d44fd75
wafaankoush99 <wafadirawe@gmail.com>
2021-03-01 01:43:48 +0200

    fixing

--- js/app.js
+++ js/app.js
@@ -2,27 +2,37 @@
 
 let hour =  ['06:00 AM','07:00 AM','08:00 AM','09:00 AM','10:00 AM','11:00 AM','12:00 PM','01:00 PM','02:00 PM','03:00 PM','04:00 PM','05:00 PM','06:00 PM','07:00 PM'];
 
+// Country function has these parameters ( name, min, max, avg, avgCookie )
 
-function Country ( name, min, max, avg, avgCookie ) {
+function Country ( name, min, max, avg ) {
   this.name = name;
   this.min = min;
   this.max = max;
   this.avg = avg;
   this.cookiesSales = [];
+  this.customerPerHour=[];
+
   this.totalSales = 0;
-  this.avgCookie = avgCookie;
+  this.totalOfTotal= 0;
+  //this.avgCookie = avgCookie;
   Country.allCountry.push( this );
 }
+
+
+
 
 Country.prototype.getSales = function () {
   for ( let i=0; i<hour.length; i++ ) {
     let cookie = Math.ceil( ( getRandomNumber ( this.min, this.max ) * this.avg ) );
     this.cookiesSales.push( cookie );
     this.totalSales += cookie;
+    this.customerPerHour.push( cookie );
+
+
   }
 };
 
-Country.prototype.render = function () {
+Country.prototype.print = function () {
   const tableElement = document.getElementById( 'placeTable' );
 
   const tr = document.createElement( 'tr' );
@@ -35,11 +45,8 @@ Country.prototype.render = function () {
   for ( let i=0 ; i< hour.length; i++ ) {
     const td2 = document.createElement( 'td' );
     tr.appendChild( td2 );
-    td2.textContent = '   ' + this.cookiesSales[i];
+    td2.textContent = this.cookiesSales[i];
   }
-
-
-
 
   const td4 = document.createElement( 'td' );
   tr.appendChild( td4 );
@@ -48,12 +55,13 @@ Country.prototype.render = function () {
 
 };
 
+
+
 const tableHeader = function() {
   const parentElement = document.getElementById( 'shops' );
   const table = document.createElement( 'table' );
   parentElement.appendChild( table );
   table.setAttribute( 'id', 'placeTable' );
-
   const tr1 = document.createElement( 'tr' );
   table.appendChild( tr1 );
 
@@ -88,102 +96,33 @@ Country.allCountry = [];
 
 const seattle = new Country ( 'Seattle', 23, 65, 6.3 );
 seattle.getSales();
-seattle.render();
+seattle.print();
 console.log( seattle );
+
 
 const tokyo = new Country ( 'Tokyo', 3, 14, 1.2 );
 tokyo.getSales();
-tokyo.render();
+tokyo.print();
 console.log( tokyo );
 
 const dubai = new Country ( 'Dubai', 11, 38, 3.7 );
 dubai.getSales();
-dubai.render();
+dubai.print();
 console.log( dubai );
 
 const paris = new Country ( 'Paris', 20, 38, 2.3 );
 paris.getSales();
-paris.render();
+paris.print();
 console.log( paris );
 
 const lima = new Country ( 'Lima', 2, 16, 4.6 );
 lima.getSales();
-lima.render();
+lima.print();
 console.log( lima );
 
 
 
 
-
-// const tableFooter = function() {
-//   const tableElement = document.getElementById( 'placeTable' );
-
-//   const tr = document.createElement( 'tr' );
-//   tableElement.appendChild( tr );
-
-//   const th1 = document.createElement( 'th' );
-//   tr.appendChild( th1 );
-//   th1.textContent = 'total';
-
-
-const addNewBranch = function() {
-  const tableElement = document.getElementById( 'placeTable' );
-
-  const tr = document.createElement( 'tr' );
-  tableElement.appendChild( tr );
-
-  const td = document.createElement( 'td' );
-  tr.appendChild( td );
-  td.textContent = this;
-};
-
-const formElement = document.getElementById( 'AddNewBranch' );
-
-
-formElement.addEventListener( 'submit' , function( event ) {
-  event.preventDefault();
-
-
-
-  const branchName = event.target.branchName.value;
-  const minNumCus = event.target.minNumCus.value;
-  const maxNumCust = event.target.maxNumCust.value;
-  const avgNum = event.target.avgNum.value;
-  //we use 'value' for the box & 'checked' for checkbox
-  //split(''); ---> take the string as it is
-  //prevent default action
-
-
-
-  const country = new Country ( branchName, minNumCus, maxNumCust, avgNum );
-
-
-
-
-
-  country.getSales();
-  country.render();
-
-  // branchName.render();
-  // minNumCus.render();
-  // maxNumCust.render();
-  // avgNum.render();
-  // console.log( branchName );
-  // console.log( minNumCus );
-  // console.log( maxNumCust );
-  // console.log( avgNum );
-
-
-  //console.log( event.target.branchName.value );
-  console.log( Country.allCountry );
-
-  formElement.reset();
-
-//console.log( 'submit' );
-} );
-
-
-addNewBranch();
 
 
 
@@ -196,7 +135,6 @@ const tableFooter = function() {
   const th1 = document.createElement( 'th' );
   tr.appendChild( th1 );
   th1.textContent = 'total';
-
   for ( let i=0; i<hour.length; i++ ) {
     const th2 = document.createElement( 'th' );
     tr.appendChild( th2 );
@@ -221,3 +159,28 @@ function getRandomNumber ( min, max ) {
 }
 
 
+
+const formElement = document.getElementById( 'AddNewBranch' );
+formElement.addEventListener( 'submit', function( event ) {
+  event.preventDefault();
+  const name = event.target.branch_Name.value;
+  const min = event.target.minNum_Cust.value;
+  const max = event.target.maxNum_Cust.value;
+  const avg = event.target.avg_Num.value;
+  if ( Number( min ) >= Number( max ) ){
+    alert( 'Invalid maximum number !! ' );
+  }
+  else{
+    document.getElementById( 'placeTable' ).removeChild( document.getElementById( 'placeTable' ).lastChild );
+    const city = new Country ( name, min, max, avg );
+    formElement.reset();
+    console.log( city.customerPerHour );
+
+    city.getSales();
+    city.print();
+
+
+    tableFooter();
+  }
+
+} );
